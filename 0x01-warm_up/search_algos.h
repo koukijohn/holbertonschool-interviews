67fc4b335ca1e2776b83042f65db49bd82a2c74f
koukijohn <koukijohn@github.com>
2018-10-08 19:32:33 +0000

    added search_algos.h

---
+++ 0x01-warm_up/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef __SEARCH_ALGOS__
+#define __SEARCH_ALGOS__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int binary_search(int *array, size_t size, int value);
+int binary_tree_is_full(const binary_tree_t *tree);
+
+#endif /* __SEARCH_ALGOS__  */
